572337dbdba35c57a75fd33eb866f293bd20b978
Leticia <lettycuevas91@gmail.com>
2023-02-17 22:45:44 -0300

    aut

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ app.set('views', 'views')
 app.use(express.urlencoded())
 
 // routes
-app.get('/', async (req, res) => res.render('login'))
-app.post("/login", async (req, res, next) => {
+//app.get('/', async (req, res) => res.render('login'))
+app.post("/", async (req, res, next) => {
     try {
       const user = await User.authenticate(req.body.email, req.body.password);
       if (user) {
         req.userId = user._id;
-        return res.redirect("/");
+        return res.redirect("/login");
       }else {
-        res.redirect("/login")
+        res.redirect("/")
       }
     } catch (e) {
       return next(e)
